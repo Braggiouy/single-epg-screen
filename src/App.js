1f5d31086b038b219f27c3ca8c13e737aa327393
Bruno Aggierni <bruno.aggierni@gmail.com>
2023-06-17 17:51:42 +0100

    Initialization of project from scratch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <h1>Hello world! I am using React</h1>
+    </>
+  );
+};
+
+export default App;
